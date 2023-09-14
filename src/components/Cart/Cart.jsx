d622961b750a0a5b00f53d78ed33d9364199a571
Taahia23 <taahiatahsin@gmail.com>
2023-09-15 04:04:30 +0600

    added total credit and remaining credit into the cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,7 +5,7 @@ const Cart = ({ selectedCourses,totalCredit, remaining }) => {
         <div className="w-2/3 bg-slate-50 p-5 rounded-xl">
 
 
-            <h5 className="text-blue-600 text-lg font-bold mb-5 mt-3">Credit Hour Remaining:{remaining} hr</h5>
+            <h5 className="text-blue-600 text-lg font-bold mb-5 mt-3">Credit Hour Remaining: {remaining} hr</h5>
             <hr className="border-1 border-black" />
 
             <h1 className=" text-2xl font-bold mb-8 mt-5">Course Name: {selectedCourses.length} </h1>
@@ -20,7 +20,7 @@ const Cart = ({ selectedCourses,totalCredit, remaining }) => {
             }
             <hr className="border-1 border-black mt-5" />
             <h3 className="text-lg font-bold mb-5 mt-3">Total Credit Hour: {totalCredit} </h3>
-            <h3 className="text-lg font-bold mb-5 mt-3">Total Price: </h3>
+            {/* <h3 className="text-lg font-bold mb-5 mt-3">Total Price: </h3> */}
 
 
         </div>
