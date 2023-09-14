89d45b4b1ab000c867c44696d4924fd143de3e76
Taahia23 <taahiatahsin@gmail.com>
2023-09-15 04:45:01 +0600

    added course total price

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -8,7 +8,7 @@ const Cart = ({ selectedCourses,totalCredit, remaining, totalPrice }) => {
             <h5 className="text-blue-600 text-lg font-bold mb-5 mt-3">Credit Hour Remaining: {remaining} hr</h5>
             <hr className="border-1 border-black" />
 
-            <h1 className=" text-2xl font-bold mb-8 mt-5">Course Name: {selectedCourses.length} </h1>
+            <h1 className=" text-2xl font-bold mb-8 mt-5">Course Name: </h1>
             {
                 //  selectedActors.map((actor) => (
                 //     <li key={actor.id}>{actor.name}</li>
